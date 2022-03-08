ec4e730520e2d75231e2839aa43998a44dbe33dd
EnDanGered <dan.name@mail.ru>
2022-03-09 00:31:40 +0100

    Add zajmeny and popesivy

--- main.py
+++ main.py
@@ -51,6 +51,56 @@ adjektiv = [[["mladý (m.a.) (sg.)"], ["mladého"], ["mladému"], ["mladého"], 
          [["jarní (f) (pl.)"], ["jarních"], ["jarním"], ["jarní"], ["jarní"], ["jarních"], ["jarními"]],
          [["jarní (n) (pl.)"], ["jarních"], ["jarním"], ["jarní"], ["jarní"], ["jarních"], ["jarními"]]]
 
+##
+
+posesivAdjektiv = [[["otcovo (n) (sg.)"], ["otcova"], ["otcovu"], ["otcovo"], ["otcovo"], ["otcově"], ["otcovým"]],
+                  [["otcova (f) (sg.)"], ["otcovy"], ["otcově"], ["otcovu"], ["otcova"], ["otcově"], ["otcovou"]],
+                  [["otcův (m.i.) (sg.)"], ["otcova"], ["otcovu"], ["otcův"], ["otcův"], ["otcově"], ["otcovým"]],
+                  [["otcův (m.a.) (sg.)"], ["otcova"], ["otcovu"], ["otcova"], ["otcův"], ["otcově"], ["otcovým"]],
+                  [["matčino (n) (sg.)"], ["matčina"], ["matčinu"], ["matčino"], ["matčino"], ["matčině"], ["matčiným"]],
+                  [["matčina (f) (sg.)"], ["matčiny"], ["matčině"], ["matčinu"], ["matčina"], ["matčině"], ["matčinou"]],
+                  [["matčin (m.i.) (sg.)"], ["matčina"], ["matčinu"], ["matčin"], ["matčin"], ["matčině"], ["matčiným"]],
+                  [["matčin (m.a.) (sg.)"], ["matčina"], ["matčinu"], ["matčina"], ["matčin"], ["matčině"], ["matčiným"]],
+                  [["otcova (n) (pl.)"], ["otcových"], ["otcovým"], ["otcova"], ["otcova"], ["otcových"], ["otcovými"]],
+                  [["otcovy (f) (pl.)"], ["otcových"], ["otcovým"], ["otcovy"], ["otcovy"], ["otcových"], ["otcovými"]],
+                  [["otcovy (m.i.) (pl.)"], ["otcových"], ["otcovým"], ["otcovy"], ["otcovy"], ["otcových"], ["otcovými"]],
+                  [["otcovi (m.a.) (pl.)"], ["otcových"], ["otcovým"], ["otcovy"], ["otcovi"], ["otcových"], ["otcovými"]],
+                  [["matčina (n) (pl.)"], ["matčiných"], ["matčiným"], ["matčina"], ["matčina"], ["matčiných"], ["matčinými"]],
+                  [["matčiny (f) (pl.)"], ["matčiných"], ["matčiným"], ["matčiny"], ["matčiny"], ["matčiných"], ["matčinými"]],
+                  [["matčiny (m.i.) (pl.)"], ["matčiných"], ["matčiným"], ["matčiny"], ["matčiny"], ["matčiných"], ["matčinými"]],
+                  [["matčini (m.a.) (pl.)"], ["matčiných"], ["matčiným"], ["matčiny"], ["matčini"], ["matčiných"], ["matčinými"]]]
+
+ukazniZajmeno = [[["ten (sg.) (m)"], ["toho"], ["tomu"], ["toho","ten"], ["NONE"], ["tom"], ["tím"]],
+                [["ta (sg.) (f)"], ["té"], ["té"], ["tu"], ["NONE"], ["té"], ["tou"]],
+                [["to (sg.) (n)"], ["toho"], ["tomu"], ["to"], ["NONE"], ["tom"], ["tím"]],
+                [["tí(ty) (pl.) (m)"], ["těch"], ["těm"], ["ty"], ["NONE"], ["těch"], ["těmi"]],
+                [["ty (pl.) (f)"], ["těch"], ["těm"], ["ty"], ["NONE"], ["těch"], ["těmi"]],
+                [["ta (pl.) (n)"], ["těch"], ["těm"], ["ta"], ["NONE"], ["těch"], ["těmi"]]]
+
+
+osobniZajmeno = [[["já (sg.)"], ["mne","mě"], ["mně","mi"], ["mne","mě"], ["NONE"], ["mně"], ["mnou"]],
+                [["ty (sg.)"], ["tebe","tě"], ["tobě","ti"], ["tebe","tě"], ["NONE"], ["tobě"], ["tebou"]],
+                [["on (sg.)"], ["jeho","ho"], ["jemu","mu"], ["jeho","ho","jej"], ["NONE"], ["něm"], ["jím"]],
+                [["ona (sg.)"], ["jí"], ["jí"], ["jí"], ["NONE"], ["ní"], ["jí"]],
+                [["ono (sg.)"], ["jeho","ho"], ["jemu","mu"], ["je"], ["NONE"], ["něm"], ["jím"]],
+                [["já (pl.)"], ["nás"], ["nám"], ["nás"], ["NONE"], ["nás"], ["námi"]],
+                [["ty (pl.)"], ["vás"], ["vám"], ["vás"], ["NONE"], ["vás"], ["vámi"]],
+                [["on (pl.)"], ["jich"], ["jim"], ["je"], ["NONE"], ["nich"], ["jimi"]],
+                [["ona (pl.)"], ["jich"], ["jim"], ["je"], ["NONE"], ["nich"], ["jimi"]],
+                [["ono (pl.)"], ["jich"], ["jim"], ["je"], ["NONE"], ["nich"], ["jimi"]]]
+
+privlastneZajmeno = [[["můj (m) (sg.)"], ["mého"], ["mému"], ["mého","můj"], ["NONE"], ["mém"], ["mým"]],
+                    [["moje(má) (f) (sg.)"], ["mé"], ["mé"], ["mou"], ["NONE"], ["mé"], ["mou"]],
+                    [["moje(mé) (n) (sg.)"], ["mého"], ["mému"], ["moje","mé"], ["NONE"], ["mém"], ["mým"]],
+                    [["náš (m) (sg.)"], ["našeho"], ["našemu"], ["našeho","náš"], ["NONE"], ["našem"], ["naším"]],
+                    [["naše (f) (sg.)"], ["naší"], ["naší"], ["naší"], ["NONE"], ["naší"], ["naší"]],
+                    [["naše (n) (sg.)"], ["našeho"], ["našemu"], ["naše"], ["NONE"], ["našem"], ["naším"]],
+                    [["moji(moje) (m) (pl.)"], ["mých"], ["mým"], ["moje","mé"], ["NONE"], ["mých"], ["mými"]],
+                    [["moje(mé) (f) (pl.)"], ["mých"], ["mým"], ["moje","mé"], ["NONE"], ["mých"], ["mými"]],
+                    [["moje(mé) (n) (pl.)"], ["mých"], ["mým"], ["moje","mé"], ["NONE"], ["mých"], ["mými"]],
+                    [["naši(naše) (m) (pl.)"], ["našich"], ["našim"], ["naše"], ["NONE"], ["našich"], ["našimi"]],
+                    [["naše (f) (pl.)"], ["našich"], ["našim"], ["naše"], ["NONE"], ["našich"], ["našimi"]],
+                    [["naše (n) (pl.)"], ["našich"], ["našim"], ["naše"], ["NONE"], ["našich"], ["našimi"]]]
 
 padList = ["NOMINATIV", "GENITIV", "DATIV", "AKUZATIV", "VOKATIV", "LOKÁL", "INSTRUMENTÁL"]
 
@@ -75,6 +125,7 @@ def MessageVzor(message, vzor: vzors):
             return
 
         if message.text == "/skip":
+            bot.send_message(message.from_user.id, str("Correct:\n" + " nebo ".join(vzor[vzorNum][padNum])))
             MessageVzor(message, vzor)
             return
 
@@ -94,7 +145,13 @@ def MessageVzor(message, vzor: vzors):
     buttons = ["/stop", "/skip"]
     keyboard.add(*buttons)
     vzorNum = random.randrange(0, len(vzor))
-    padNum = random.randrange(1, 6)
+    if ["NONE"] in vzor[vzorNum]:
+        if random.random() > 0.5:
+            padNum = random.randint(5, len(vzor[vzorNum])-1)
+        else:
+            padNum = random.randint(1, 3)
+    else:
+        padNum = random.randrange(1, len(vzor[vzorNum])-1)
 
     bot.send_message(message.from_user.id,
                      str("Print vzor: \n" + " nebo ".join(vzor[vzorNum][0]) + "\n in pad: \n" + str(padList[padNum])), reply_markup=keyboard)
@@ -102,10 +159,27 @@ def MessageVzor(message, vzor: vzors):
 
 def CheangeVzorVariant(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Substantiv", "Adjektiv", "Zajmeny"]
+    buttons = ["Substantiv", "Adjektiv", "Zajmeny", "Posesiv"]
     keyboard.add(*buttons)
     bot.send_message(message.from_user.id, "Vyberte jednu z možností", reply_markup=keyboard)
     bot.register_next_step_handler(message, GetVzorVarriant)
+
+def CheangeZajmVariant(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["Ukazni", "Osobni", "Privlastne"]
+    keyboard.add(*buttons)
+    bot.send_message(message.from_user.id, "Vyberte jednu z možností", reply_markup=keyboard)
+    bot.register_next_step_handler(message, GetZajmVariant)
+
+def GetZajmVariant(message):
+    if message.text == "Ukazni":
+        MessageVzor(message, ukazniZajmeno)
+    elif message.text == "Osobni":
+        MessageVzor(message, osobniZajmeno)
+    elif message.text == "Privlastne":
+        MessageVzor(message, privlastneZajmeno)
+    else:
+        return
 
 
 def GetVzorVarriant(message):
@@ -113,8 +187,10 @@ def GetVzorVarriant(message):
         MessageVzor(message, vzors)
     elif message.text == "Adjektiv":
         MessageVzor(message, adjektiv)
-  #  elif message.text == "Zajmeny":
-   #     MessageVzor(message, zaymeny)
+    elif message.text == "Posesiv":
+        MessageVzor(message, posesivAdjektiv)
+    elif message.text == "Zajmeny":
+        CheangeZajmVariant(message)
     else:
         return
 
